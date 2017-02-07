910d59362623c3b9459242fc60adb799dc3375e7
Corneezius <cfr2116@columbia.edu>
2017-02-07 04:00:51 -0500

    add connectivity service that checks to make sure user is online before rendering map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -25,5 +25,113 @@ export class HomePage {
      this.loadGoogleMaps();
    }
 
-   
+  //  loadGoogleMaps checks to see if google obect is available, which means SDK has loaded
+
+   loadGoogleMaps(){
+
+    this.addConnectivityListeners();
+
+  if(typeof google == "undefined" || typeof google.maps == "undefined"){
+
+    console.log("Google maps JavaScript needs to be loaded.");
+    this.disableMap();
+
+    // If the SDK has not been loaded then we check if the user is online and then we load it by injecting the script into the document
+    if(this.connectivityService.isOnline()){
+      console.log("online, loading map");
+
+      //Load the SDK, trigger callback function so we know its safe to start
+      window['mapInit'] = () => {
+        this.initMap();
+        this.enableMap();
+      }
+
+      let script = document.createElement("script");
+      script.id = "googleMaps";
+
+      if(this.apiKey){
+        script.src = 'http://maps.google.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
+      } else {
+        script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';
+      }
+
+      document.body.appendChild(script);
+
+    }
+  }
+  else {
+
+    if(this.connectivityService.isOnline()){
+      console.log("showing map");
+      this.initMap();
+      this.enableMap();
+    }
+    else {
+      console.log("disabling map");
+      this.disableMap();
+    }
+
+  }
+
+  }
+
+  initMap(){
+
+    this.mapInitialised = true;
+
+    Geolocation.getCurrentPosition().then((position) => {
+
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    });
+
+  }
+
+  disableMap(){
+    console.log("disable map");
+  }
+
+  enableMap(){
+    console.log("enable map");
+  }
+
+// Listen for when users come back online
+  addConnectivityListeners(){
+
+    let onOnline = () => {
+
+      setTimeout(() => {
+        if(typeof google == "undefined" || typeof google.maps == "undefined"){
+
+          this.loadGoogleMaps();
+
+        } else {
+
+          if(!this.mapInitialised){
+            this.initMap();
+          }
+
+          this.enableMap();
+        }
+      }, 2000);
+
+    };
+
+    let onOffline = () => {
+      this.disableMap();
+    };
+
+    document.addEventListener('online', onOnline, false);
+    document.addEventListener('offline', onOffline, false);
+
+  }
+
 }
