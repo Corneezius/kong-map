a18d947a430fd503d5e3b0ab94c7423a0eb56198
Corneezius <cfr2116@columbia.edu>
2017-02-07 02:00:06 -0500

    add geolocation plugin, use built in position coordinates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
 
 // created a member variable to hold map
 declare var google;
@@ -24,15 +25,21 @@ export class HomePage {
 
   loadMap(){
 
-    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+     Geolocation.getCurrentPosition().then((position) => {
+
+    // create a LatLng object to represent location on map
+    let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
     let mapOptions = {
       center: latLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
-
+    // mapOptions allows customization,i.e, zoom level, type of map
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
+      }, (err) => {
+     console.log(err);
+    });
   }
 }
