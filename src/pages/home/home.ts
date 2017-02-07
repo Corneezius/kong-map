cd1a537b34eb82b769f1ab658aa2e65ad4da10a4
Corneezius <cfr2116@columbia.edu>
2017-02-07 01:51:57 -0500

    update home.ts to hold map and create latLng object with googlemap api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+// created a member variable to hold map
+declare var google;
+
 @Component({
-  selector: 'page-home',
+  selector: 'home-page',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  // created a member variable that references the map element
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
-    
+
   }
 
+  ionViewLoaded(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
 }
