65bb0df60c060b91864e55a0a3273f1a927240a8
Corneezius <cfr2116@columbia.edu>
2017-02-08 02:49:36 -0500

    move function that generates map after service to google-maps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,173 +19,180 @@ export class HomePage {
 
 
 
-// import { Component } from '@angular/core';
-// import { MapPage } from '../map/map';
-// import { ListPage } from '../list/list';
-
-
-// import { Component, ViewChild, ElementRef } from '@angular/core';
-// import { NavController } from 'ionic-angular';
-// import { ConnectivityService } from '../../providers/connectivity-service';
-// import { Geolocation } from 'ionic-native';
+import { Injectable } from '@angular/core';
+import { ConnectivityService } from '../../providers/connectivity-service';
+import { Geolocation } from 'ionic-native';
 
 
 // created a member variable to hold map
-// declare var google;
 
-// @Component({
-//   selector: 'page-home',
-//   templateUrl: 'home.html'
-// })
-//
-// export class HomePage {
-//
-//   tab1Root: any = MapPage;
-//   tab2Root: any = ListPage;
-//
-//   constructor() {
-//
-//   }
-// }
+declare var google;
 
+@Injectable()
+export class GoogleMaps {
 
-// export class HomePage {
-//
-//   // created a member variable that references the map element
-//   @ViewChild('map') mapElement: ElementRef;
-//
-//  map: any;
-//  mapInitialised: boolean = false;
-//  apiKey: any;
-//
-// // loadGoogleMaps function triggers logic
-//  constructor(public nav: NavController, public connectivityService: ConnectivityService) {
-//      this.loadGoogleMaps();
-//    }
-//
-//   //  loadGoogleMaps checks to see if google obect is available, which means SDK has loaded
-//
-//    loadGoogleMaps(){
-//
-//     this.addConnectivityListeners();
-//
-//   if(typeof google == "undefined" || typeof google.maps == "undefined"){
-//
-//     console.log("Google maps JavaScript needs to be loaded.");
-//     this.disableMap();
-//
-//     // If the SDK has not been loaded then we check if the user is online and then we load it by injecting the script into the document
-//     if(this.connectivityService.isOnline()){
-//       console.log("online, loading map");
-//
-//       //Load the SDK, trigger callback function so we know its safe to start
-//       window['mapInit'] = () => {
-//         this.initMap();
-//         this.enableMap();
-//       }
-//
-//       let script = document.createElement("script");
-//       script.id = "googleMaps";
-//
-//       if(this.apiKey){
-//         script.src = 'http://maps.google.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
-//       } else {
-//         script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';
-//       }
-//
-//       document.body.appendChild(script);
-//
-//     }
-//   }
-//   else {
-//
-//     if(this.connectivityService.isOnline()){
-//       console.log("showing map");
-//       this.initMap();
-//       this.enableMap();
-//     }
-//     else {
-//       console.log("disabling map");
-//       this.disableMap();
-//     }
-//
-//   }
-//
-//   }
-//   // initializes map
-//   initMap(){
-//
-//     this.mapInitialised = true;
-//
-//     Geolocation.getCurrentPosition().then((position) => {
-//
-//       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-//
-//       let mapOptions = {
-//         center: latLng,
-//         zoom: 15,
-//         mapTypeId: google.maps.MapTypeId.ROADMAP
-//       }
-//
-//       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-//
-//     });
-//
-//   }
-//
-//   addMarker(){
-//
-//   let marker = new google.maps.Marker({
-//     map: this.map,
-//     animation: google.maps.Animation.DROP,
-//     position: this.map.getCenter()
-//   });
-//
-//   let content = "<h4>Information!</h4>";
-//
-//   this.addInfoWindow(marker, content);
-//
-//   }
-//
-//   disableMap(){
-//     console.log("disable map");
-//   }
-//
-//   enableMap(){
-//     console.log("enable map");
-//   }
-//
-// // Listen for when users come back online
-//   addConnectivityListeners(){
-//
-//     let onOnline = () => {
-//
-//       setTimeout(() => {
-//         if(typeof google == "undefined" || typeof google.maps == "undefined"){
-//
-//           this.loadGoogleMaps();
-//
-//         } else {
-//
-//           if(!this.mapInitialised){
-//             this.initMap();
-//           }
-//
-//           this.enableMap();
-//         }
-//       }, 2000);
-//
-//     };
-//
-//     let onOffline = () => {
-//       this.disableMap();
-//     };
-//
-//     document.addEventListener('online', onOnline, false);
-//     document.addEventListener('offline', onOffline, false);
-//   }
+  mapElement: any;
+  pleaseConnect: any;
+  map: any;
+  mapInitialised: boolean = false;
+  mapLoaded: any;
+  mapLoadedObserver: any;
+  markers: any = [];
+  apiKey: string;
 
+  constructor(public connectivityService: ConnectivityService) {
 
+  }
 
+  init(mapElement: any, pleaseConnect: any): Promise<any> {
 
-// }
+    this.mapElement = mapElement;
+    this.pleaseConnect = pleaseConnect;
+
+    return this.loadGoogleMaps();
+
+  }
+
+  loadGoogleMaps(): Promise<any> {
+
+    return new Promise((resolve) => {
+
+      if(typeof google == "undefined" || typeof google.maps == "undefined"){
+
+        console.log("Google maps JavaScript needs to be loaded.");
+        this.disableMap();
+
+        if(this.connectivityService.isOnline()){
+
+          window['mapInit'] = () => {
+
+            this.initMap().then(() => {
+              resolve(true);
+            });
+
+            this.enableMap();
+          }
+
+          let script = document.createElement("script");
+          script.id = "googleMaps";
+
+          if(this.apiKey){
+            script.src = 'http://maps.google.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
+          } else {
+            script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';
+          }
+
+          document.body.appendChild(script);
+
+        }
+      }
+      else {
+
+        if(this.connectivityService.isOnline()){
+          this.initMap();
+          this.enableMap();
+        }
+        else {
+          this.disableMap();
+        }
+
+      }
+
+      this.addConnectivityListeners();
+
+    });
+
+  }
+
+  initMap(): Promise<any> {
+
+    this.mapInitialised = true;
+
+    return new Promise((resolve) => {
+
+      Geolocation.getCurrentPosition().then((position) => {
+
+        // UNCOMMENT FOR NORMAL USE
+        //let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        let latLng = new google.maps.LatLng(40.713744, -74.009056);
+
+        let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        this.map = new google.maps.Map(this.mapElement, mapOptions);
+        resolve(true);
+
+      });
+
+    });
+
+  }
+
+  disableMap(): void {
+
+    if(this.pleaseConnect){
+      this.pleaseConnect.style.display = "block";
+    }
+
+  }
+
+  enableMap(): void {
+
+    if(this.pleaseConnect){
+      this.pleaseConnect.style.display = "none";
+    }
+
+  }
+
+  addConnectivityListeners(): void {
+
+    document.addEventListener('online', () => {
+
+      console.log("online");
+
+      setTimeout(() => {
+
+        if(typeof google == "undefined" || typeof google.maps == "undefined"){
+          this.loadGoogleMaps();
+        }
+        else {
+          if(!this.mapInitialised){
+            this.initMap();
+          }
+
+          this.enableMap();
+        }
+
+      }, 2000);
+
+    }, false);
+
+    document.addEventListener('offline', () => {
+
+      console.log("offline");
+
+      this.disableMap();
+
+    }, false);
+
+  }
+
+  addMarker(lat: number, lng: number): void {
+
+    let latLng = new google.maps.LatLng(lat, lng);
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng
+    });
+
+    this.markers.push(marker);
+
+  }
+
+}
